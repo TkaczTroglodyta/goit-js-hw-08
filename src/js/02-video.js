77ea3d3033fa516632cc5b72cb10e8b27dd43583
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-25 00:03:00 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,43 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const player = new Player('vimeo-player', {});
+
+// const play = ({ seconds }) => {
+//   try {
+//     localStorage.setItem('videoplayer-current-time', seconds);
+//   } catch (err) {
+//     console.log(err.name);
+//   }
+// };
+
+// player.on('timeupdate', throttle(play, 1000));
+
+// player
+//   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+//   .then(seconds => {
+//     console.log(`The current time is ${seconds} seconds.`);
+//   });
+
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import Throttle from 'lodash.throttle';
 
-const player = new Player('vimeo-player', {});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEYCURRENTTIME = 'videoplayer-current-time';
 
-const play = ({ seconds }) => {
-  try {
-    localStorage.setItem('videoplayer-current-time', seconds);
-  } catch (err) {
-    console.log(err.name);
+let isTimeNow = localStorage.getItem(KEYCURRENTTIME);
+
+function CurrentTime(e) {
+  localStorage.setItem(KEYCURRENTTIME, e.seconds);
+}
+
+function getCurrentTime(e) {
+  if (isTimeNow) {
+    player.setCurrentTime(isTimeNow);
+    player.off('play', getCurrentTime);
   }
-};
+  player.on('timeupdate', Throttle(CurrentTime, 1000));
+}
 
-player.on('timeupdate', throttle(play, 1000));
-
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(seconds => {
-    console.log(`The current time is ${seconds} seconds.`);
-  });
+player.on('play', getCurrentTime);
