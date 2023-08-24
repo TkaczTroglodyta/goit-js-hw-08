bcc18926809736b8383b57d18736fcfcafa9004d
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-24 23:19:08 +0200

    feat: new dependencies & task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {});
+
+const play = ({ seconds }) => {
+  try {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  } catch (err) {
+    console.log(err.name);
+  }
+};
+
+player.on('timeupdate', throttle(play, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(seconds => {
+    console.log(`The current time is ${seconds} seconds.`);
+  });
