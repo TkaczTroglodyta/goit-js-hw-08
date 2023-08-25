e9d0c896367e7ff11b710413c4b62ab3c9e428b0
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-25 18:44:23 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,13 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+
 const player = new Player('vimeo-player', {});
 
 const play = ({ seconds }) => {
   try {
-    localStorage.setItem('videoplayer-current-time', seconds);
+    localStorage.setItem(TIME_KEY, seconds);
   } catch (err) {
     console.log(err.name);
   }
@@ -13,8 +15,6 @@ const play = ({ seconds }) => {
 
 player.on('timeupdate', throttle(play, 1000));
 
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(seconds => {
-    console.log(`The current time is ${seconds} seconds.`);
-  });
+player.setCurrentTime(localStorage.getItem(TIME_KEY)).then(seconds => {
+  console.log(`The current time is ${seconds} seconds.`);
+});
