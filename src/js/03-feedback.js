d672df1c89521600b6e8dc2709d5cf6480e8a2b1
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-26 13:03:55 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,42 @@
+// import throttle from 'lodash.throttle';
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+// const onDataInput = e => {
+//   dataForm = { email: email.value, message: message.value };
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+// };
+
+// const onFormSubmit = e => {
+//   e.preventDefault();
+//   console.log({ email: email.value, message: message.value });
+
+//   if (email.value === '' || message.value === '') {
+//     return alert(`Please fill in all required fields.`);
+//   }
+
+//   localStorage.removeItem(STORAGE_KEY);
+//   e.currentTarget.reset();
+//   dataForm = {};
+// };
+
+// const pageReload = () => {
+//   if (dataForm) {
+//     email.value = dataForm.email || '';
+//     message.value = dataForm.message || '';
+//   }
+// };
+
+// const fbackForm = document.querySelector('.feedback-form');
+// const { email, message } = fbackForm.elements;
+
+// fbackForm.addEventListener('input', throttle(onDataInput, 500));
+// fbackForm.addEventListener('submit', onFormSubmit);
+
+// pageReload();
+
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
@@ -29,10 +68,17 @@ const pageReload = () => {
   }
 };
 
-const fbackForm = document.querySelector('.feedback-form');
-const { email, message } = fbackForm.elements;
+const init = () => {
+  const fbackForm = document.querySelector('.feedback-form');
 
-fbackForm.addEventListener('input', throttle(onDataInput, 500));
-fbackForm.addEventListener('submit', onFormSubmit);
+  if (fbackForm !== null) {
+    const { email, message } = fbackForm.elements;
 
-pageReload();
+    fbackForm.addEventListener('input', throttle(onDataInput, 500));
+    fbackForm.addEventListener('submit', onFormSubmit);
+
+    pageReload();
+  }
+};
+
+window.addEventListener('load', init);
