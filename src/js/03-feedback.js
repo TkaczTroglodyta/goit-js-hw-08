5fa829d31041b42f135431999d4546b1df801b98
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-26 13:25:15 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,103 +4,13 @@ const STORAGE_KEY = 'feedback-form-state';
 
 let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-// const onDataInput = e => {
-//   dataForm = { email: email.value, message: message.value };
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
-// };
-
-// const onFormSubmit = e => {
-//   e.preventDefault();
-//   console.log({ email: email.value, message: message.value });
-
-//   if (email.value === '' || message.value === '') {
-//     return alert(`Please fill in all required fields.`);
-//   }
-
-//   localStorage.removeItem(STORAGE_KEY);
-//   e.currentTarget.reset();
-//   dataForm = {};
-// };
-
-// const pageReload = () => {
-//   if (dataForm) {
-//     email.value = dataForm.email || '';
-//     message.value = dataForm.message || '';
-//   }
-// };
-
-// const fbackForm = document.querySelector('.feedback-form');
-// const { email, message } = fbackForm.elements;
-
-// fbackForm.addEventListener('input', throttle(onDataInput, 500));
-// fbackForm.addEventListener('submit', onFormSubmit);
-
-// pageReload();
-
-// not working either
-
-// const onDataInput = e => {
-//   dataForm = { email: email.value, message: message.value };
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
-// };
-
-// const onFormSubmit = e => {
-//   e.preventDefault();
-//   console.log({ email: email.value, message: message.value });
-
-//   if (email.value === '' || message.value === '') {
-//     return alert(`Please fill in all required fields.`);
-//   }
-
-//   localStorage.removeItem(STORAGE_KEY);
-//   e.currentTarget.reset();
-//   dataForm = {};
-// };
-
-// const pageReload = () => {
-//   if (dataForm) {
-//     email.value = dataForm.email || '';
-//     message.value = dataForm.message || '';
-//   }
-// };
-
-// const init = () => {
-//   const fbackForm = document.querySelector('.feedback-form');
-
-//   if (fbackForm !== null) {
-//     const { email, message } = fbackForm.elements;
-
-//     fbackForm.addEventListener('input', throttle(onDataInput, 500));
-//     fbackForm.addEventListener('submit', onFormSubmit);
-
-//     pageReload();
-//   }
-// };
-
-// window.addEventListener('load', init);
-
-// cant understand an error :(
-
-form.addEventListener('input', throttle(onInputData, 500));
-form.addEventListener('submit', onFormSubmit);
-
-const { email, message } = form.elements;
-reloadPage();
-
-function onInputData(event) {
+const onDataInput = e => {
   dataForm = { email: email.value, message: message.value };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
-}
+};
 
-function reloadPage() {
-  if (dataForm) {
-    email.value = dataForm.email || '';
-    message.value = dataForm.message || '';
-  }
-}
-
-function onFormSubmit(event) {
-  event.preventDefault();
+const onFormSubmit = e => {
+  e.preventDefault();
   console.log({ email: email.value, message: message.value });
 
   if (email.value === '' || message.value === '') {
@@ -108,6 +18,21 @@ function onFormSubmit(event) {
   }
 
   localStorage.removeItem(STORAGE_KEY);
-  event.currentTarget.reset();
+  e.currentTarget.reset();
   dataForm = {};
-}
+};
+
+const pageReload = () => {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+};
+
+const fbackForm = document.querySelector('.feedback-form');
+const { email, message } = fbackForm.elements;
+
+fbackForm.addEventListener('input', throttle(onDataInput, 500));
+fbackForm.addEventListener('submit', onFormSubmit);
+
+pageReload();
