91a2edbbc45f109bfe8c627cd6d0050032cdd40d
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-27 17:38:37 +0200

    fix: check localStorage & time set

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,8 +6,13 @@ const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = e => localStorage.setItem('TIME_KEY', e.seconds);
-const currentTime = localStorage.getItem('TIME_KEY');
+const onPlay = playTime => {
+  const { seconds } = playTime;
+  localStorage.setItem(TIME_KEY, seconds);
+  console.log(`Video current time is ${seconds} seconds.`);
+};
+
+const currentTime = localStorage.getItem(TIME_KEY);
 
 currentTime ? player.setCurrentTime(currentTime) : null;
 
