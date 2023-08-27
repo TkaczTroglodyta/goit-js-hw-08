301415bd77e1a3217c7e6657febfe8a391809907
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-27 08:43:29 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,45 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const TIME_KEY = 'videoplayer-current-time';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// let isAboutTime = localStorage.getItem(TIME_KEY);
+
+// const currentTime = e => {
+//   localStorage.setItem(TIME_KEY, e.seconds);
+// };
+
+// const getCurrentTime = e => {
+//   if (isAboutTime) {
+//     player.setCurrentTime(isAboutTime);
+//     player.off('play', getCurrentTime);
+//   }
+//   player.on('timeupdate', throttle(currentTime, 1000));
+// };
+
+// player.on('play', getCurrentTime);
+// console.log(`Video current time is ${isAboutTime} seconds.`);
+
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const TIME_KEY = 'videoplayer-current-time';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const player = new Player('vimeo-player', {});
 
-let isAboutTime = localStorage.getItem(TIME_KEY);
-
-const currentTime = e => {
-  localStorage.setItem(TIME_KEY, e.seconds);
-};
-
-const getCurrentTime = e => {
-  if (isAboutTime) {
-    player.setCurrentTime(isAboutTime);
-    player.off('play', getCurrentTime);
+const play = ({ seconds }) => {
+  try {
+    localStorage.setItem(TIME_KEY, seconds);
+  } catch (err) {
+    console.log(err.name);
   }
-  player.on('timeupdate', throttle(currentTime, 1000));
 };
 
-player.on('play', getCurrentTime);
-console.log(`Video current time is ${isAboutTime} seconds.`);
+player.on('timeupdate', throttle(play, 1000));
+
+player.setCurrentTime(localStorage.getItem(TIME_KEY)).then(seconds => {
+  console.log(`The current time is ${seconds} seconds.`);
+});
